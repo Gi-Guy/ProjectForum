0071b4800a03c60d8d9c7f321b75ded237e414e6
Guy <Guy9502@Gmail.com>
2021-07-27 17:45:44 +0300

    Fixing a small bug.

--- src/main/java/com/projectForum/topic/TopicRepository.java
+++ src/main/java/com/projectForum/topic/TopicRepository.java
@@ -1,7 +1,11 @@
 package com.projectForum.topic;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TopicRepository extends JpaRepository<Topic, Integer> {
+import com.projectForum.user.User;
 
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+	Set<Topic> findByUser(User user);
 }
