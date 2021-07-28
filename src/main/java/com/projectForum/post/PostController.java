5aab40161b12656ad95c388ef586d5e5ef4eea59
Guy <Guy9502@Gmail.com>
2021-07-28 13:49:40 +0300

    Created a temp new topic page
    *Doing some testing of creating a new topic.
    -There is no topic page (only creating new topic)
    -There is a bug of Authentication.

--- src/main/java/com/projectForum/post/PostController.java
+++ src/main/java/com/projectForum/post/PostController.java
@@ -2,7 +2,17 @@ package com.projectForum.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * This controller will handle the next actions:
+ * Find all Posts by id 
+ * Delete a post by id
+ * */
+
 
 @Controller
 @RequestMapping(value = "/post")
@@ -13,6 +23,13 @@ public class PostController {
 	@Autowired
 	public PostController(PostRepository postRepository) {
 		this.postRepo = postRepository;
+	}
+	
+	@GetMapping("/{username}")
+	public String getAllPostsByUsername(@PathVariable String username, Model model) {
+		
+		// TODO finish this method.
+		return "";
 	}
 	
 }
