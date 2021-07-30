5015d9831272157d9e88fba65fde2c89cb8cf544
Guy <Guy9502@Gmail.com>
2021-07-30 16:02:46 +0300

    Relocate addNewPost to postController

--- src/main/java/com/projectForum/post/PostController.java
+++ src/main/java/com/projectForum/post/PostController.java
@@ -1,11 +1,19 @@
 package com.projectForum.post;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.projectForum.topic.TopicRepository;
+import com.projectForum.user.UserRepository;
 
 /**
  * This controller will handle the next actions:
@@ -14,20 +22,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * */
 
 @Controller
-@RequestMapping(value = "/post")
+//@RequestMapping(value = "/post")
 public class PostController {
 	
+	
+	private UserRepository 	userRepo;
 	private PostRepository postRepo;
+	private TopicRepository topicRepo;
 	
+
 	@Autowired
-	public PostController(PostRepository postRepository) {
-		this.postRepo = postRepository;
+	public PostController(UserRepository userRepo, PostRepository postRepo, TopicRepository topicRepo) {
+		super();
+		this.userRepo = userRepo;
+		this.postRepo = postRepo;
+		this.topicRepo = topicRepo;
 	}
-	
-	@GetMapping("/{username}")
-	public String getAllPostsByUsername(@PathVariable String username, Model model) {
+
+
+
+	/**This method will add a new post to an exists topic*/
+	@PostMapping("/topic/{topicId}")
+	public String addNewPost(@Valid @ModelAttribute Post post, BindingResult bindingResult, @PathVariable int topicId,
+								Authentication authentication, Model model) {
+		// If hasErrors == true, then return to topic page.
+		if(bindingResult.hasErrors()) {
+			System.out.println("I'm here (New post)  and I don't know what to do with my life.");
+			// TODO: Solve this issue <------------------------------------------------------FIX THIS!
+		}
 		
-		// TODO finish this method.
-		return "";
+		// No errors, creating a new post in topic
+		// TODO check if new post register dates.
+		post.setUser(userRepo.findByUsername(authentication.getName()));
+		post.setTopic(topicRepo.findTopicById(topicId));
+		postRepo.save(post);
+
+		model.asMap().clear();//Cleaning model because it don't some weird things if not.
+		return "redirect:/topic/" + topicId; //User will return to the same topic page
 	}
 }
