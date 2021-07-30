f6cd2926f4731969591b34bf958d8c0ee12411e5
Guy <Guy9502@Gmail.com>
2021-07-30 14:19:52 +0300

    Trying to solve conflicts.

--- src/main/java/com/projectForum/index/IndexController.java
+++ src/main/java/com/projectForum/index/IndexController.java
@@ -1,22 +1,17 @@
 package com.projectForum.index;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.projectForum.user.User;
 import com.projectForum.user.UserRepository;
 
 @Controller
 public class IndexController {
 	
-	
 	/**
-	 * 
 	 * This controller is temporary and only exists for testing purpose.
 	 * It will be deactivate and removed when Home controller will be ready.*/
 	// Autowired - Spring framework automatically creates a new instance of user repository and inject it into this class
@@ -37,7 +32,10 @@ public class IndexController {
 	
 	//This method has been moved to userController
 	/**
-	 * This method creates a page of all users */
+	 * This method creates a page of all users 
+	 * */
+	
+	
 	/*
 	@GetMapping("/list_users")
 	public String listofUsers(Model model) {
@@ -45,8 +43,9 @@ public class IndexController {
 	    //model.addAttribute("listofUsers", listofUsers);
 		model.addAttribute("listofUsers", userRepo.findAll());
 	    return "users";
-	}
-	*/
+	}*/
+	
+	
 	/**
 	 * This method will delete a user in the users list by button active */
 	@GetMapping("/deleteUser")
